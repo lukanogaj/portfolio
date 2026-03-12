63d15a4e0343579924005a45ec0594fa1aee6ba5
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-03-12 11:08:38 +0000

    feat: add Container layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Container from "./components/Container/Container";
+
 function App() {
 	return (
-		<>
+		<Container>
 			<h1>Lukasz Nogaj</h1>
 			<p>Frontend Developer</p>
-		</>
+		</Container>
 	);
 }
 
