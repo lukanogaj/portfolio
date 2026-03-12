4b390e89b08b00aaec1515024bc7f8f15dd0b383
Lukasz Nogaj <lukasz.nogajdev@gmail.com>
2026-03-12 11:36:06 +0000

    feat: add ProjectCard component and render first project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./sections/Hero/Hero";
-
+import ProjectCard from "./components/ProjectCard/ProjectCard";
+import { projects } from "./data/projects";
 const App = () => {
 	return (
 		<>
 			<Hero />
+			<ProjectCard project={projects[0]} />
 		</>
 	);
 };
---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,41 @@
+import styles from "./ProjectCard.module.scss";
+
+const ProjectCard = ({ project }) => {
+	const { title, description, stack, github, demo } = project;
+
+	return (
+		<article className={styles.card}>
+			<h3 className={styles.title}>{title}</h3>
+
+			<p className={styles.description}>{description}</p>
+
+			<div className={styles.stack}>
+				{stack.map((tech) => (
+					<span
+						key={tech}
+						className={styles.tech}>
+						{tech}
+					</span>
+				))}
+			</div>
+
+			<div className={styles.links}>
+				<a
+					href={github}
+					target='_blank'
+					rel='noopener noreferrer'>
+					GitHub
+				</a>
+
+				<a
+					href={demo}
+					target='_blank'
+					rel='noopener noreferrer'>
+					Live Demo
+				</a>
+			</div>
+		</article>
+	);
+};
+
+export default ProjectCard;
